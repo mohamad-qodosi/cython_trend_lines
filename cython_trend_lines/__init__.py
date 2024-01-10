fbfc3dcab7a6d7e4ba95541bfa18c3a4f502d2b8
mohamad qodosi <mohamadqodosi1996@gmail.com>
2024-01-10 16:02:42 +0330

    fix: :hammer: Add low_trend_lines to __init__.

--- cython_trend_lines/__init__.py
+++ cython_trend_lines/__init__.py
@@ -1,4 +1,4 @@
 import numpy as np
-from cython_trend_lines.trend_lines import high_trend_lines
+from cython_trend_lines.trend_lines import high_trend_lines, low_trend_lines
 
-__all__ = ['high_trend_lines']
+__all__ = ['high_trend_lines', 'low_trend_lines']
